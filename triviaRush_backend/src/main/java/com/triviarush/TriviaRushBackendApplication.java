a60e8aa9cbe31b9b476a12461d7cb88995d6eb19
Juan José <jurrilo.25.22.github@gmail.com>
2025-07-16 01:58:50 +0200

    feat: structure backend

---
+++ triviaRush_backend/src/main/java/com/triviarush/TriviaRushBackendApplication.java
@@ -0,0 +1,13 @@
+package com.triviarush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaRushBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriviaRushBackendApplication.class, args);
+    }
+
+}
---
+++ triviaRush_backend/src/test/java/com/triviarush/TriviaRushBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triviarush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaRushBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
